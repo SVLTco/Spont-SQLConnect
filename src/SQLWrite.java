4c0d3ab14666c4b0982784f77ab9219d53a37dbc
Matthew Tighe <mjt6124@rit.edu>
2020-02-09 07:10:10 -0500

    write for adding friends to lists

--- src/SQLWrite.java
+++ src/SQLWrite.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.bcel.internal.ExceptionConst;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Properties;
@@ -34,15 +36,42 @@ public class SQLWrite {
         return user;
     }
 
-    /**
-    public static boolean createGroup(SpontUser user, String groupName, int userIDs[] ){
-        for(int)
+    public static boolean createGroup(SQLConnection database, SpontUser user, String groupName, int userIDs[] ) {
+        try {
+            database.getStatement().executeUpdate("CREATE TABLE Group_" + user.getUserID() + "_" + (user.getGroups().size() + 1) + " (userID VARCHAR(255));");
 
-    }*/
+            for (int i = 0; i < userIDs.length; i++) {
+                database.getStatement().executeUpdate("INSERT INTO Group_" + user.getUserID() + "_" + (user.getGroups().size() + 1) + " (userID) values (" + userIDs[i] + ");");
+            }
+            user.addGroup(new SQLGroup(user.getUserID(), groupName));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean addFriend(SQLConnection database, SpontUser user, int userID) {
+        try {
+            database.getStatement().executeUpdate("INSERT INTO Friends_" + user.getUserID() + " (userID) values (userID)");
+        } catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         SQLConnection db = new SQLConnection("35.243.223.126", "root", "A7eqjnaejqqsgOII");
 
-        addUser(db, 2, "test2", "pw", (float) 40.00, (float) 4.034043030, 0);
+        SpontUser clientside1 = addUser(db, 1, "test1", "pw", (float) 40.00, (float) 4.034043030, 0);
+        SpontUser clientside2 = addUser(db, 2, "test2", "pw", (float) 40.00, (float) 4.034043030, 0);
+        SpontUser clientside3 = addUser(db, 3, "test3", "pw", (float) 40.00, (float) 4.034043030, 0);
+        SpontUser clientside4 = addUser(db, 4, "test4", "pw", (float) 40.00, (float) 4.034043030, 0);
+
+        int users[] = new int[]{1, 2, 3};
+        System.out.println(createGroup(db, clientside4, "SALT", users));
 
         db.closeConnection();
     }
